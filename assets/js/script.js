dcfcee5836469be2175f789f7f839be56fab20d9
sharon bunyasi <on6azmsu@student.codeinstitute.net>
2025-04-23 02:17:40 +0200

    Add the Dom elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//DOM elements
+let button = document.querySelectorAll('.choice');
+let resultMessage = document.getElementById('result-message');
+let playerScoreDisplay = document.getElementById('player-score');
+let computerScoreDisplay = document.getElementById('computer-score');
+let resetButton = document.getElementById('reset');
+let playerChoiceDisplay = document.getElementById('player-choice');
+let computerChoiceDisplay = document.getElementById('computer-choice');
+let choices = ['rock','paper','scissors','lizard','spock'];
+let playerScore = 0;
+let computerScore = 0;
